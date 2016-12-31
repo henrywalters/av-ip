724364481020fc64aba4c32ff4717b2372185622
henrywalters <henrywalters20@gmail.com>
2016-12-30 20:58:31 -0500

    Rewrapped

---
+++ README.md
@@ -0,0 +1,8 @@
+#AVIP
+
+## Summary
+AVIP is a lightweight ruby gem for geolocation of ip addresses. The gem is a wrapper built around ipinfo.io. 
+$$ Installation
+```
+gem install av-ip
+````
--- lib/av-ip.rb
+++ lib/av-ip.rb
@@ -1,36 +1,50 @@
 require 'net/https'
 require 'uri'
 
+
 class AVIP
-	def initialize(ip = '127.0.0.1', format = 'json')
+	def initialize(ip = '127.0.0.1', content = [])
 		@ip = ip
-		@format = format
-		@formats = [
-			'xml',
-			'csv',
-			'json'
-		]
+		@content = content
 		@params = [
-			'format',
-			'ip'
+			:format,
+			:ip,
+			:content
+		]
+
+		@contents = [
+			:ip,
+			:hostname,
+			:city,
+			:region,
+			:country,
+			:loc,
+			:org,
+			:postal
 		]
 	end
 
-	# Input options in hash form ie {:format => 'json', :ip => '127.0.0.1'}
+	# Input options in hash form ie {:format => :json, :ip => '127.0.0.1'}
 	def update(param_hash)
 		param_hash.each do |key,value|
-			if @params.include?(key.to_s)
-				if key.to_s == 'format'
-					if @formats.include?(value.to_s)
-						@format = value
-						puts "Format changed to #{value}."
-					else
-						puts "Format type does not exist."
-						puts "#{key} has not changed."
-					end
-				elsif key.to_s =='ip'
+			if @params.include?(key)
+				if key == :ip
 					@ip = value
 					puts "Ip changed to #{value}."
+				elsif key == :content
+					if value.length == 0
+						puts "Invalid Input, using all outputs"
+					else
+						content = []
+						value.each do |val|
+							if @contents.include?(val)
+								@content.push(val)
+								puts "Including: #{val}"
+							else
+								puts "#{val} is not a valid option."
+							end
+						end
+					end
 				end
 			else
 				puts "Paramater: #{key} is not a valid option."
@@ -45,11 +59,28 @@ class AVIP
 		else
 			p_ip = ip
 		end
-		uri = URI("http://freegeoip.net/#{@format}/#{p_ip}")
+		uri = URI("http://ipinfo.io/#{p_ip}/#{@format}")
 		Net::HTTP.start(uri.host, uri.port) do |http|
 			request = Net::HTTP::Get.new uri
 			response = http.request request
-			return response.body
+			res = eval(response.body )
+			if @content == []
+				return res
+			else
+				new_res = {}
+				res.each do |key,value|
+					@content.each do |con|
+						if key == con
+							new_res[key] = value
+						end
+					end
+				end
+				return new_res
+			end
 		end
 	end
 end
+
+i = AVIP.new()
+i.update( { :ip => '66.87.114.81', :content => [:city,:region, :loc]} )
+puts i.search
